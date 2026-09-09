34e55732d0095e31edb2dc4feb8e5b55335771a2
GiuliaTeixeira3626 <giulia.teixeira@sptech.school>
2026-09-09 16:31:15 -0300

    Removendo data de nascimento do funcionário

--- script-de-captura-grupo.py
+++ script-de-captura-grupo.py
@@ -7,8 +7,8 @@ from mysql.connector import connection
 db = connection.MySQLConnection(
     host="localhost",
     port=3306,
-    user="ec2",
-    passwd="ec2123",
+    user="root",
+    passwd="primeiromac",
     database="grupo10"
 )
 
